dca0faf25e6fb5884e1ce08fac5b611d0ac22379
RiyaGuha1234 <donaguhachoudhury@gmail.com>
2021-05-16 11:30:30 +0530

    DB error rectified

--- coder-angular/src/app/shared/GlobalVariable.ts
+++ coder-angular/src/app/shared/GlobalVariable.ts
@@ -1,11 +1,16 @@
 
- let url;
- if (window.location.origin === '127.0.0.1'){
-   url = window.location.origin;
- }
+ let BASE_URL;
 
+ if (window.location.origin === 'http://localhost:4200'){
+   BASE_URL = 'http://127.0.0.1';
+ }
+ else{
+   BASE_URL = window.location.origin;
+ }
  export const GlobalVariable = Object.freeze({
-  API_URL: 'http://127.0.0.1/coder_project/coder_api/public/api/',
+  // API_URL: 'http://127.0.0.1/coder_project/coder_api/public/api/',
+   API_URL: BASE_URL + '/coder_project/coder_api/public/api/',
+
   // API_URL: 'http://127.0.0.1:8000/api/',
 
   // ... more of your variables
